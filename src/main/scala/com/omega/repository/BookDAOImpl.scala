1323d3a773cd75d8fc2d633fb8874b526bfa9b6a
Milind <milind.chawla86@gmail.com>
2016-03-31 16:08:19 +0530

    Small changes

--- src/main/scala/com/omega/repository/BookDAOImpl.scala
+++ src/main/scala/com/omega/repository/BookDAOImpl.scala
@@ -4,17 +4,20 @@ import java.util.{ List => JList }
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.DependsOn
-import org.springframework.jdbc.core.BeanPropertyRowMapper
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
 import com.omega.domain.Book
 
 import javax.persistence.Entity
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
+import javax.persistence.Table
+
 
 @Repository("BookDAO")
+@Transactional
 @DependsOn(Array("BookService"))
 class BookDAOImpl extends BookDAO {
     
@@ -22,10 +25,11 @@ class BookDAOImpl extends BookDAO {
     private var jdbcTemplate: JdbcTemplate = _
     
     @PersistenceContext
-    private var entityManager: EntityManager = _ 
+    private var entityManager: EntityManager = _
     
     def getBooks: JList[Book] = {
-        //jdbcTemplate.query("SELECT ID, NAME FROM BOOK", new BeanPropertyRowMapper(classOf[Book]))
+        // jdbcTemplate.query("SELECT ID, NAME FROM BOOK", new BeanPropertyRowMapper(classOf[Book]))
+        // println(slickDatabase)
         entityManager.createQuery("SELECT b FROM Book b").getResultList.asInstanceOf[JList[Book]]
     }
 }
