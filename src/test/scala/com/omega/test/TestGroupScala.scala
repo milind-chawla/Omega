9406b1c9b820e55906d5391434cbdeacfb3fe0a6
Milind <milind.chawla86@gmail.com>
2016-04-07 23:29:44 +0530

    Grouping with TCO

--- src/test/scala/com/omega/test/TestGroupScala.scala
+++ src/test/scala/com/omega/test/TestGroupScala.scala
@@ -2,13 +2,11 @@ package com.omega.test
 
 object TestGroupScala {
     def main(args: Array[String]): Unit = {
-        val i = List[Int]() :: Nil
-        println(i)
-        
-        println(groupNumbers(List(1, 2, 3, 4, 5, 9, 11, 20, 21, 22)))
+        println(groupNumbersRec(List(1, 2, 3, 4, 5, 9, 11, 20, 21, 22))((i, j) => i - j == 1))
     }
     
     def groupNumbers(list: List[Int]): List[List[Int]] = {
+        
         def groupThem(lst: List[Int], acc: List[Int]): List[List[Int]] = lst match {
             case Nil => acc.reverse :: Nil
             case x :: xs => acc match {
@@ -19,5 +17,20 @@ object TestGroupScala {
         }
         
         groupThem(list, List())
+    }
+    
+    def groupNumbersRec(list: List[Int])(f: (Int, Int) => Boolean): List[List[Int]] = {
+        
+        @scala.annotation.tailrec
+        def groupThem(lst: List[Int], result: List[List[Int]], acc: List[Int]): List[List[Int]] = lst match {
+            case Nil => acc.reverse :: result
+            case x :: xs => acc match {
+                case Nil => groupThem(xs, result, x :: acc)
+                case y :: ys if(f(x, y)) => groupThem(xs, result, x :: acc)
+                case k => groupThem(xs, acc.reverse :: result, x :: List())
+            }
+        }
+        
+        groupThem(list, List(), List()).reverse
     }
 }
