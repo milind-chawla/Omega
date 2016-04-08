abb8ec1ca326ec5f02aa1bc6633310c8c3e78529
Milind <milind.chawla86@gmail.com>
2016-04-08 13:38:21 +0530

    Chapter 1

--- src/test/scala/com/omega/test/Hello1.scala
+++ src/test/scala/com/omega/test/Hello1.scala
@@ -1,8 +1,23 @@
 package com.omega.test
 
-object TestGroupScala {
+object Hello1 {
     def main(args: Array[String]): Unit = {
-        println(groupNumbersRec(List(1, 2, 3, 4, 5, 9, 11, 20, 21, 22))((i, j) => i - j == 1))
+        var list = List(1, 2, 3, 4, 5, 9, 11, 20, 21, 22)
+        
+        println( groupNumbersRec(list, (i, j) => i - j == 1) )
+        println( groupNumbersSz(list, (i, j) => j - i == 1) )
+        
+        list = List(1, 3, 4, 6, 8, 9)
+        
+        println( groupNumbersSz(list, (i, j) => j - i == 2) )
+        
+        println("*" * 30)
+        
+        val d1 = List(1, 2, 3, 4, 5)
+        val d2 = List(11, 22, 33, 44, 55)
+        
+        println( (d1, d2).zipped map (_ + _) )
+        println( (1 to 100).map(2*).filter { x => x%3==0 && x%4==0 && x%5==0 } )
     }
     
     def groupNumbers(list: List[Int]): List[List[Int]] = {
@@ -19,7 +34,7 @@ object TestGroupScala {
         groupThem(list, List())
     }
     
-    def groupNumbersRec(list: List[Int])(f: (Int, Int) => Boolean): List[List[Int]] = {
+    def groupNumbersRec(list: List[Int], f: (Int, Int) => Boolean): List[List[Int]] = {
         
         @scala.annotation.tailrec
         def groupThem(lst: List[Int], result: List[List[Int]], acc: List[Int]): List[List[Int]] = lst match {
@@ -32,5 +47,11 @@ object TestGroupScala {
         }
         
         groupThem(list, List(), List()).reverse
+    }
+    
+    def groupNumbersSz(list: List[Int], f: (Int, Int) => Boolean): List[_] = {
+        import scalaz.syntax.std.list._
+        
+        list groupWhen f
     }
 }
