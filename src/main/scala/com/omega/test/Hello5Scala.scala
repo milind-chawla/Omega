35fc6ce0936deb97d25c56783f63930031d1d812
Milind <milind.chawla86@gmail.com>
2016-04-11 23:57:13 +0530

    Streaming

--- src/main/scala/com/omega/test/Hello5Scala.scala
+++ src/main/scala/com/omega/test/Hello5Scala.scala
@@ -12,10 +12,63 @@ object Hello5Scala {
     }
     
     def main(args: Array[String]): Unit = {
-        import ReverseList._
+        import HelloHelpers._
         
-        val l = 1 to 20000 toList
+        noexec {
+            import ReverseList._
         
-        println(reverseList(l, Nil))
+            val l = 1 to 20000 toList
+        
+            println(reverseList(l, Nil))    
+        }
+        
+        noexec {
+            lazy val p = {
+                println("Initializing")
+                9
+            }
+            
+            println(p) 
+            println(p)
+        }
+        
+        noexec {
+            var k = 2
+            def f() = { k += 1; k }
+            lazy val p = Stream.continually(f())
+            
+            p take 4 foreach println
+            println(p)
+        }
+        
+        noexec {
+            lazy val r = Stream.cons(1, Stream.cons(2, Stream.Empty))
+            r take 4 foreach println
+        }
+        
+        noexec {
+            def s(n: Int): Stream[Int] = Stream.cons(n, s(n + 1))
+            lazy val q = s(0)
+            
+            println(q)
+            
+            def succ(n: Int): Stream[Int] = n #:: succ(n + 1)
+            lazy val r = succ(0)
+            
+            println(r)
+        }
+        
+        exec {
+            var k = 3
+            def f() = {
+                println("prevouys value = " + k)
+                k += 1
+                k
+            }
+            val p = Stream.continually(f())
+            
+            p take 3 foreach println
+            p take 3 foreach println
+        }
     }
 }
