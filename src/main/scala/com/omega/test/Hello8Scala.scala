840f4d63b2488bcec845de202d0c3e90247658e0
Milind <milind.chawla86@gmail.com>
2016-04-18 16:03:04 +0530

    Task

--- src/main/scala/com/omega/test/Hello8Scala.scala
+++ src/main/scala/com/omega/test/Hello8Scala.scala
@@ -41,7 +41,7 @@ object Hello8Scala {
             println(order2.price)    
         }
         
-        exec {
+        noexec {
             object Palindrome {
                 def findp(s: String): Vector[(String, (Int, Int))] = {
                     var v = Vector.empty[(String, (Int, Int))]
@@ -69,6 +69,71 @@ object Hello8Scala {
             }
             
             println(v.maxBy(f => f._2._2 - f._2._1))
+        }
+        
+        noexec {
+            val f = (x: Int, y: Int) => x / y
+            
+            println(f(12, 3))
+            //println(f(12, 0))
+            
+            val g: PartialFunction[(Int, Int), Int] = {
+                case (i, j) if j != 0 => i / j
+            }
+            
+            println(g.isDefinedAt(10, 0))
+            println(g.isDefinedAt(10, 10))
+            
+            println(g(10, 5))
+        }
+        
+        noexec {
+            val empList = List(("Roy", 100), ("Sunil", 200), ("Atul", 120))
+            
+            println(
+                empList filter (e => e._2 > 100) map (e => e._1)
+            )
+            
+            println(
+                empList filter { case (_, bonus) => bonus > 100 } map { case (name, _) => name }
+            )
+        }
+        
+        noexec {
+            val f: PartialFunction[Int, Int] = { case i if i > 0 => i * i }
+            
+            println( f.lift(10) )
+            println( f.lift(-1) )
+        }
+        
+        exec {
+            case class Task(description: String)
+            
+            type TaskHandler = PartialFunction[Task, Unit]
+            
+            def canHandle(task: Task, phrases: String*): Boolean = phrases exists (task.description.toLowerCase.contains(_))
+            def handleIt(name: String, task: Task): Unit = println(s"${name} Handling: " + task.description)
+            
+            val dad: TaskHandler = {
+                case task: Task if canHandle(task, "wood", "hunt") => handleIt("Dad", task)
+            }
+            
+            val mom: TaskHandler = {
+                case task: Task if canHandle(task, "sew", "cook") => handleIt("Mom", task)
+            }
+            
+            val kid: TaskHandler = {
+                case task: Task if canHandle(task, "dog", "cat") => handleIt("Kid", task)
+            }
+            
+            val f = dad orElse mom orElse kid
+            
+            f(Task("Feed the cat"))
+            
+            val taskList = List("sew up a shirt button", "Walk the Dog", "cut some firewood", "feed the cat", "feed the parrot")
+            
+            taskList map (Task(_)) collect f
+            taskList map (Task(_)) flatMap { x => f.lift(x) }
         }
     }
 }
