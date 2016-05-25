3f529d5a8f4bc4d232404713d300913722ccf8bb
Milind Chawla <milind.chawla86@gmail.com>
2016-05-25 23:25:59 +0530

    Some javascript added

--- WebContent/resources/javascript/books.js
+++ WebContent/resources/javascript/books.js
@@ -2,7 +2,18 @@
 	$("#" + disable + " > a").attr('href', 'javascript:void');
 	$("#" + disable).addClass("disabled");
 	
+	$('.book-show').on('click', function(e) {
+		e.preventDefault();
+		bootbox.alert('Show: ' + $(this).attr('href'));
+	});
+	
+	$('.book-edit').on('click', function(e) {
+		e.preventDefault();
+		bootbox.alert('Edit: ' + $(this).attr('href'));
+	});
+	
 	$('.book-delete').on('click', function(e) {
+		e.preventDefault();
 		bootbox.alert('Delete: ' + $(this).attr('href'));
 	});
 })(jQuery);
